f0541e8c4baf9907f605725f5f0d784a1f7bfc8b
Luiz Gustavo C Setten <luizgustavosetten@hotmail.com>
2020-09-10 23:06:42 -0300

    fix: Informacoes com aspas duplas detalhes evento

--- mobile/jornada/src/telas/detalhesEvento/detalhesEvento.js
+++ mobile/jornada/src/telas/detalhesEvento/detalhesEvento.js
@@ -7,6 +7,7 @@ import Estilos from './estilo';
 function detalhesEvento({route}) {
   const {nomeEvento} = route.params;
   const {nomeResposaveis} = route.params;
+  console.log(nomeResposaveis);
   const {sinopse} = route.params;
 
   return (
@@ -16,15 +17,11 @@ function detalhesEvento({route}) {
           <View style={Estilos.card}>
             <View style={Estilos.event}>
               <Text style={Estilos.heads}>Título</Text>
-              <Text style={Estilos.simpleText}>
-                {JSON.stringify(nomeEvento)}
-              </Text>
+              <Text style={Estilos.simpleText}>{nomeEvento}</Text>
               <Text style={Estilos.heads}>Responsáveis</Text>
-              <Text style={Estilos.simpleText}>
-                {JSON.stringify(nomeResposaveis)}
-              </Text>
+              <Text style={Estilos.simpleText}>{nomeResposaveis}</Text>
               <Text style={Estilos.heads}>Sinopse </Text>
-              <Text style={Estilos.simpleText}>{JSON.stringify(sinopse)}</Text>
+              <Text style={Estilos.simpleText}>{sinopse}</Text>
             </View>
           </View>
           <Text style={Estilos.heads}>Insira a chave para fazer check-in </Text>
